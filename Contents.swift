06888d31308d9c8fe3276d33af3f3aaa225e22a9
PegahSmiley26 <PegahSmiley@gmail.com>
2019-07-09 15:30:23 -0500

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,87 @@
+////////import UIKit
+////////
+//////////thsi is an array bc it's only storing one thing in each element
+////////var sponsors = ["Adidas", "Este Lauder", "Good Girl", "Apple", "WeWork"]
+////////
+//////////I am going to write a loop that prints all of our sponsors
+//////////for each sponsor in the array sponsors, I am going to print their names
+//////////sponsor is a new variable I made up so I can go through my array  in order
+////////for sponsor in sponsors{
+////////    print("Shout out to \(sponsor) for helping make KWK possible :D")
+////////}
+//////////prints the statements with each sponsors
+////////
+//////////now printing a dictionary
+//////var capitals = ["France" : "Paris", "Cuba" : "Havana", "Japan" : "Tokyo"]
+//////////for capital in capitals{
+//////////    print(capital)
+//////////}
+////////
+//////////now we want to print keys and values separately
+////////for (country, capital) in capitals {
+////////    //print countries only and capitals only
+////////    print(country)
+////////    print(capital)
+////////}
+////////
+////////
+////////
+////////for in loop that prints keys and values separately but only uses ONE variable
+//////for pair in capitals{
+//////    //i want to access the country names
+//////    print(pair.key) //dont types keys, just key
+//////    //bc all of the country names are values
+//////
+//////    print(pair.value)
+//////}
+//////
+//////
+////////Array
+//////var friends = ["Aileen", "Cady", "Halle", "Michelle", "Hunter"]
+//////
+////////Dictionary
+//////var describingFriends = ["Aileen" : "kind", "Cady" : "smart", "Halle" : "sweet", "Michelle" : "cute", "Hunter" : "energetic"]
+//////for (person, adj) in describingFriends {
+//////print("\(person) is \(adj)")
+//////}
+//////
+//////
+//////
+//////
+//////
+//////
+//////
+//////printing something a certian amount of times
+////for _ in 1...4 {
+////    print("Hey")
+////
+////}
+////for _ in 1...999 {
+////    print("Soutout to Portland KWK Scholars!")
+////}
+////
+////
+////Will come back later
+////say i have two different arrays and assume they are the same size (store the same amount of info)
+//var arrayOne : [String] = []
+//var arrayTwo : [String] = []
+//
+////declare a dictionary so that stuff in arrayOne becomes the keys and the location becomes the values
+//var dictionary : [String : String] = [:]
+//
+//for (index, element) in arrayOne.enumerated(){
+//    //
+//    dictionary[element]=arrayOne[index]
+//
+//}
+////this shows the command called enumerated
+//
+//
+//
+//
+//
+//
+
+
+
+
